e0de1ded459dac7883a25d5dd6f1904bef211fa9
lsalvatierra <luisangel.salvat@gmail.com>
2017-08-04 10:59:05 -0500

    Commit inicial

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Areas/Seguridad/Controllers/PrincipalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESANMSA.Areas.Seguridad.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Seguridad/Principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ESAN.EDU.PE.MSA/ESANMSA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESANMSA.MvcApplication" Language="C#" %>
