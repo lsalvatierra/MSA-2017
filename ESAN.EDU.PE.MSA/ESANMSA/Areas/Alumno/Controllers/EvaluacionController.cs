a0889d30b993ea34ad2d6626ed49a31b1504236f
lsalvatierra <luisangel.salvat@gmail.com>
2017-08-07 11:22:01 -0500

    asddasd

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Areas/Alumno/Controllers/EvaluacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESANMSA.Areas.Alumno.Controllers
+{
+    public class EvaluacionController : Controller
+    {
+        // GET: Alumno/Evaluacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
