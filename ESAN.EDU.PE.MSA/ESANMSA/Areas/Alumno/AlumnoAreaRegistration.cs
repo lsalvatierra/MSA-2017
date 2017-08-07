91d822a5901895925525231b80aaea3b7adceaed
lsalvatierra <luisangel.salvat@gmail.com>
2017-08-07 12:10:55 -0500

    actualizacion

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Areas/Alumno/AlumnoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ESANMSA.Areas.Alumno
+{
+    public class AlumnoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Alumno";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Alumno_default",
+                "Alumno/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
