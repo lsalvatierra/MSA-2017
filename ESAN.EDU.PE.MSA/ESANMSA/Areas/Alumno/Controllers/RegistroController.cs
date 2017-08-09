a20b61d8ae8be9249cde782944da23d5dd6ae4e4
LuisAngel <LuisAngel@LUISANGEL>
2017-08-09 12:27:11 -0500

    commit lsalvatierra

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Areas/Alumno/Controllers/RegistroController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ESAN.Componentes.CoreEvaluacion.Logic.Facade.EvaluacionMSA;
+using ESAN.Componentes.CoreEvaluacion.Models.General.EvaluacionMSA;
+
+namespace ESANMSA.Areas.Alumno.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Alumno/Registro
+        public ActionResult Index()
+        {
+            ViewBag.lstTipoDoc = DATipoDocumento.ListaTipoDocumentos();
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult VerificarUsuario(int p_idTipoDocumento, string p_nroDocumento)
+        {
+            bool rptaExiste = false;
+            Participante objParticipante = DAParticipante.ObtenerParticipante(p_idTipoDocumento, p_nroDocumento);
+            if(objParticipante != null)
+            {
+                rptaExiste = true;
+            }
+            return Json(new { Existe = rptaExiste });
+        }
+    }
+}
