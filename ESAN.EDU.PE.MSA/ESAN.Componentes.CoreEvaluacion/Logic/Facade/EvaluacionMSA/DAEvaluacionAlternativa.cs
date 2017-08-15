ddaf3075fe7ee8202896ac73126eaf9404ad223a
Luis Chang <luis.chang@outlook.com>
2017-08-15 12:22:06 -0500

    Commit Facade

---
+++ ESAN.EDU.PE.MSA/ESAN.Componentes.CoreEvaluacion/Logic/Facade/EvaluacionMSA/DAEvaluacionAlternativa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAN.Componentes.CoreEvaluacion.Models.General.EvaluacionMSA;
+
+
+namespace ESAN.Componentes.CoreEvaluacion.Logic.Facade.EvaluacionMSA
+{
+    public class DAEvaluacionAlternativa
+    {
+        /// <summary>
+        /// Obtener R
+        /// </summary>
+        /// <param name="p_idPromocion"></param>
+        /// <param name="p_idMedicion"></param>
+        /// <param name="p_idParticipante"></param>
+        /// <param name="p_idPregunta"></param>
+        /// <param name="p_idParticipanteEval"></param>
+        /// <returns></returns>
+        public static EvaluacionRespuesta ObtenerRespuestadeEvaluacion(int p_idPromocion, int p_idMedicion, int p_idParticipante, int p_idPregunta, int p_idParticipanteEval)
+        {
+            EvaluacionRespuesta objEvaluacionRpta = null;
+            using (var data = new BDEvaluacionEntities())
+            {
+                if (p_idParticipanteEval == 0)
+                {
+                    objEvaluacionRpta = data.EvaluacionRespuesta.Where(q => q.EvaluacionPromocionID == p_idPromocion && q.EvaluacionMedicionID == p_idMedicion && q.ParticipanteID == p_idParticipante && q.EvaluacionAlternativa.EvaluacionPreguntaID == p_idPregunta).FirstOrDefault();
+                }
+                else {
+                    objEvaluacionRpta = data.EvaluacionRespuesta.Where(q => q.EvaluacionPromocionID == p_idPromocion && q.EvaluacionMedicionID == p_idMedicion && q.ParticipanteID == p_idParticipante && q.EvaluacionAlternativa.EvaluacionPreguntaID == p_idPregunta && q.ParticipanteEvaluadoID == p_idParticipanteEval).FirstOrDefault();
+                }
+                
+            }
+            return objEvaluacionRpta;
+        }
+    }
+}
---
+++ ESAN.EDU.PE.MSA/ESAN.Componentes.CoreEvaluacion/Logic/Facade/EvaluacionMSA/DAEvaluacionNivel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAN.Componentes.CoreEvaluacion.Models.General.EvaluacionMSA;
+using System.Data.Entity;
+
+
+namespace ESAN.Componentes.CoreEvaluacion.Logic.Facade.EvaluacionMSA
+{
+    public class DAEvaluacionNivel
+    {
+        /// <summary>
+        /// Lista de Niveles de evaluación .
+        /// </summary>
+        /// <param name="p_idPromocion">Id Promoción.</param>
+        /// <param name="p_idNivelPadre">Id Nivel padre.</param>
+        /// <returns>Lista de niveles de evaluaciones.</returns>
+        public static List<EvaluacionNivel> ListaNivelesxEvaluacion(int p_idPromocion, int p_idNivelPadre)
+        {
+            List<EvaluacionNivel> lstEvaluacionNivel = null;
+            using (var data = new BDEvaluacionEntities())
+            {
+                var promocion = data.EvaluacionPromocion.Find(p_idPromocion);
+
+                lstEvaluacionNivel = data.EvaluacionNivel.Include(a => a.EvaluacionNivel1.Select(b => b.EvaluacionNivel1.Select(c => c.EvaluacionNivel1))).Where(q => q.EvaluacionID == promocion.EvaluacionID && q.EvaluacionNivelPadreID == p_idNivelPadre && q.EvaluacionNivelEstado == true).OrderBy(q => q.EvaluacionNivelOrden).ToList();
+               
+                data.Configuration.LazyLoadingEnabled = false;
+            }
+            return lstEvaluacionNivel;
+        }
+
+    }
+}
