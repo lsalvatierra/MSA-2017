e58fba05b2ee27a9b415c33af4a715edff693621
LuisAngel <LuisAngel@LUISANGEL>
2017-08-15 12:03:01 -0500

    Cambios lsalvatierra.

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Scripts/Areas/Alumno/Registro/Index.js
@@ -0,0 +1,116 @@
+﻿$(document).ready(function () {
+    $("#btnVerificarDatos").on("click", function () {
+        var formValidado = true;
+        var nroDocumento = $("#txtNroDocumento").val();
+        if (nroDocumento == "") {
+            MostrarMensajeError("xnrodoc", "Ingrese su código de alumno.");
+            formValidado = false;
+        } else {
+            LimpiarMensajeError("xnrodoc");
+        }
+        if (formValidado) {
+            waitingDialog.show('Cargando', { dialogSize: 'sm', progressType: 'danger' });
+            $.ajax({
+                type: "POST",
+                url: URL_PAGE + "Alumno/Registro/VerificarUsuario",
+                cache: false,
+                data: {
+                    p_idTipoDocumento: 7,
+                    p_nroDocumento: nroDocumento
+                },
+                success: function (data) {                   
+                    if (data.Existe) {
+                        $("#frmRegistro").submit();
+                    } else {
+                        $("#divRegistrar").fadeIn("slow");
+                    }
+                    waitingDialog.hide();
+                }
+            });
+        }
+    });
+
+    $("#btnGuardarEmpezar").on("click", function () {
+        var formValidado = true;
+        var nroDocumento = $("#txtNroDocumento").val();
+        var apePaterno = $("#txtApellidoPaterno").val();
+        var apeMaterno = $("#txtApellidoMaterno").val();
+        var nombres = $("#txtNombres").val();
+
+        if (apePaterno == "") {
+            MostrarMensajeError("xapepat", "Ingrese su apellido paterno.");
+            formValidado = false;
+        } else 
+            LimpiarMensajeError("xapepat");
+        
+        if (apeMaterno == "") {
+            MostrarMensajeError("xapemat", "Ingrese su apellido materno.");
+            formValidado = false;
+        } else 
+            LimpiarMensajeError("xapemat");
+
+        if (nombres == "") {
+            MostrarMensajeError("xnom", "Ingrese sus nombres.");
+            formValidado = false;
+        } else
+            LimpiarMensajeError("xnom");
+
+
+
+        if (formValidado) {
+            //alert("vamos a registrar");
+            waitingDialog.show('Cargando', { dialogSize: 'sm', progressType: 'danger' });
+            $.ajax({
+                type: "POST",
+                url: URL_PAGE + "Alumno/Registro/RegistrarUsuario",
+                cache: false,
+                data: {
+                    p_idTipoDocumento: 7,
+                    p_nroDocumento: nroDocumento,
+                    p_apePaterno: apePaterno,
+                    p_apeMaterno: apeMaterno,
+                    p_nombres: nombres,
+                    p_idPromocion: $("#hddIdPromocion").val(),
+                    p_idMedicion: $("#hddIdMedicion").val(),
+                    p_Externo: $("#hddExterno").val()
+                },
+                success: function (data) {
+                    if (data.rpta) {
+                        $("#frmRegistro").submit();
+                    } else {
+                      alert("Ocurrió un error en la base de datos.")
+                    }
+                    waitingDialog.hide();
+                }
+            });
+
+        } 
+    });
+
+    $("#txtApellidoPaterno").keyup(function () {
+        if ($("#xapepat").html().length > 0) {
+            LimpiarMensajeError("xapepat");
+        }        
+    });
+    $("#txtApellidoMaterno").keyup(function () {
+        if ($("#xapemat").html().length > 0) {
+            LimpiarMensajeError("xapemat");
+        }
+    });
+    $("#txtNombres").keyup(function () {
+        if ($("#xnom").html().length > 0) {
+            LimpiarMensajeError("xnom");
+        }
+    });
+});
+
+// funcion para mostrar mensaje de error
+function MostrarMensajeError(div, mensaje) {
+    LimpiarMensajeError(div);
+    $("#" + div).append("<h6 class='text-danger'><span class='glyphicon glyphicon-asterisk' aria-hidden='true'></span>&nbsp;" + mensaje + "</h6>");
+    $("#" + div).hide().removeClass('hide').slideDown();
+}
+// funcion que limpia el mensaje de error
+function LimpiarMensajeError(div) {
+    $("#" + div).empty();
+}
