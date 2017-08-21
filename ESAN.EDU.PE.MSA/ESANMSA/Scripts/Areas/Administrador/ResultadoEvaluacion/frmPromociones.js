b047f929f09e8fd0ef448a324326fcb9f621d3cc
LuisAngel <LuisAngel@LUISANGEL>
2017-08-21 16:12:41 -0500

    lsalvatierra

---
+++ ESAN.EDU.PE.MSA/ESANMSA/Scripts/Areas/Administrador/ResultadoEvaluacion/frmPromociones.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("[data-toggle='tooltip']").tooltip();
+    
+    $(".btnVerParticipantes").on("click", function () {
+        //alert($(this).attr("data-idPromocion"));
+        waitingDialog.show('Cargando', { dialogSize: 'sm', progressType: 'danger' });
+        $.ajax({
+            type: "POST",
+            url: URL_PAGE + "Administrador/ResultadoEvaluacion/ParticipantesPromocion",
+            cache: false,
+            data: {
+                p_idPromocion: $(this).attr("data-idPromocion")
+            },
+            success: function (data) {
+                $("#divfrmResultado").html("");
+                $("#divfrmResultado").html(data);
+                waitingDialog.hide();
+            }
+        });
+    });
+
+    
+
+});
