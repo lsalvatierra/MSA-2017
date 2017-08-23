34aa50b1ebe6b138158f6b4c7cbb6a44ec7b5b21
LuisAngel <LuisAngel@LUISANGEL>
2017-08-23 09:53:06 -0500

    Datos Usuario

---
+++ ESAN.EDU.PE.MSA/ESAN.Componentes.CoreEvaluacion/Logic/Facade/EvaluacionMSA/DAEvaluacionUsuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAN.Componentes.CoreEvaluacion.Models.General.EvaluacionMSA;
+
+
+namespace ESAN.Componentes.CoreEvaluacion.Logic.Facade.EvaluacionMSA
+{
+    public class DAEvaluacionUsuario
+    {
+
+
+        /// <summary>
+        /// Obtener al usuario del sistema.
+        /// </summary>
+        /// <param name="p_usuario">Tipo de documento.</param>
+        /// <param name="p_password">Nro de documento</param>
+        /// <returns>Objeto Usuario</returns>
+        public static EvaluacionUsuario ObtenerUsuario(string p_usuario, string p_password)
+        {
+            using (var data = new BDEvaluacionEntities())
+            {
+                return data.EvaluacionUsuario.Where(q => q.CodUsuario == p_usuario && q.PassUsuario == p_password).FirstOrDefault();
+            }
+        }
+    }
+}
